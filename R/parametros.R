b0e8e42df78a4515cc2082d53918b66d7be976ee
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2018-12-08 17:54:23 -0300

    Agrega conjunto de parámetros de prueba por conveniencia

--- R/parametros.R
+++ R/parametros.R
@@ -1,3 +1,12 @@
+### PARÁMETROS DE PRUEBA
+# alfa <- .05
+# rango_k <- c(20, 50)
+# rango_n <- c(1, 5)
+# rango_p <- c(0.9, 0.99)
+# n_sims <- 2
+
+
+### PARÁMETROS PRODUCTIVOS
 alfa <- .05
 rango_k <- c(20, 50, 100, 500)
 rango_n <- c(1, 5, 20, 50, 100)
